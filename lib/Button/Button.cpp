4f8fb3f5423b1472c3af30558fab52b1b452c45e
SpyTigro <robbe.wuyts06@gmail.com>
2025-03-19 15:25:24 +0100

    update button.cpp

--- lib/Button/Button.cpp
+++ lib/Button/Button.cpp
@@ -1,12 +1,5 @@
 #include "Button.h"
 
-#ifdef UNIT_TEST
-static long fakeMillis = 0;
-long millis() { return fakeMillis; }
-static int fakeButtonState = HIGH;
-int digitalRead(int pin) { return fakeButtonState; }
-#endif
-
 Button::Button(byte pin)
 {
     this->pin = pin;
