2268bd3e9011db77c18a0d405b79e049fa78cabf
SpyTigro <robbe.wuyts06@gmail.com>
2025-04-03 21:35:06 +0200

    changed enc encBtn and lcd to be manually allocated

--- src/main.cpp
+++ src/main.cpp
@@ -20,20 +20,20 @@
 #define LCD_D5 9
 #define LCD_D6 8
 #define LCD_D7 7
-LiquidCrystal lcd = LiquidCrystal(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+LiquidCrystal *lcd = new LiquidCrystal(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
 // initialize encoder
 #define ENC_BTN 4
 #define ENC_PIN_A 2
 #define ENC_PIN_B 3
-Encoder enc = Encoder(ENC_PIN_A, ENC_PIN_B);
-Button encBtn = Button(ENC_BTN);
+Encoder *enc = new Encoder(ENC_PIN_A, ENC_PIN_B);
+Button *encBtn = new Button(ENC_BTN);
 
 VolumeTracker *volumes[5] = {new VolumeTracker(VOLUME_NAME_1), new VolumeTracker(VOLUME_NAME_2), new VolumeTracker(VOLUME_NAME_3), new VolumeTracker(VOLUME_NAME_4), new VolumeTracker(VOLUME_NAME_5)};
 bool areInputs[5] = {VOLUME_1_IS_INPUT, VOLUME_2_IS_INPUT, VOLUME_3_IS_INPUT, VOLUME_4_IS_INPUT, VOLUME_5_IS_INPUT};
 
-Page *pages[PAGE_AMOUNT] = {new OverviewPage("Volumes", &lcd, &encBtn, &enc, volumes, areInputs),
-							new VolumePage("Mic", &lcd, &encBtn, &enc, volumes[4], areInputs[4])};
+Page *pages[PAGE_AMOUNT] = {new OverviewPage("Volumes", lcd, encBtn, enc, volumes, areInputs),
+							new VolumePage("Mic", lcd, encBtn, enc, volumes[4], areInputs[4])};
 unsigned pageIdx = 0;
 Page *curPage = pages[pageIdx]->load();
 
@@ -41,7 +41,7 @@ void sendVolumeValues();
 
 void setup()
 {
-	lcd.begin(16, 2);
+	lcd->begin(16, 2);
 
 	Serial.begin(115200);
 }
