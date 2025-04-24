372fb19cef6f496c10da85137955f5924cebbd83
SpyTigro <robbe.wuyts06@gmail.com>
2025-04-24 14:25:22 +0200

    forgot to reformat these 2

--- PIO-Code/src/util/VolumeTracker.cpp
+++ PIO-Code/src/util/VolumeTracker.cpp
@@ -1,40 +1,21 @@
 #include "VolumeTracker.h"
 
-VolumeTracker::VolumeTracker(String name, uint8_t volume, bool mute) : name(name),
-                                                                       volume(volume),
-                                                                       muted(mute)
-{
-}
+VolumeTracker::VolumeTracker(String name, uint8_t volume, bool mute)
+    : name(name), volume(volume), muted(mute) {}
 
-String VolumeTracker::getName()
-{
-    return name;
-}
+String VolumeTracker::getName() { return name; }
 
-void VolumeTracker::setName(String name)
-{
-    this->name = name;
-}
+void VolumeTracker::setName(String name) { this->name = name; }
 
-uint8_t VolumeTracker::getVolume()
-{
-    return volume;
-}
+uint8_t VolumeTracker::getVolume() { return volume; }
 
-bool VolumeTracker::isMuted()
-{
-    return muted;
-}
+bool VolumeTracker::isMuted() { return muted; }
 
-void VolumeTracker::addVolume(int8_t value)
-{
-    if ((int8_t)volume + value < 0)
-    {
+void VolumeTracker::addVolume(int8_t value) {
+    if ((int8_t)volume + value < 0) {
         volume = 0;
         return;
-    }
-    else if (volume + value > 100)
-    {
+    } else if (volume + value > 100) {
         volume = 100;
         return;
     }
@@ -42,24 +23,13 @@ void VolumeTracker::addVolume(int8_t value)
     return;
 }
 
-void VolumeTracker::setVolume(uint8_t volume)
-{
-    if (0 > volume && volume > 100)
-        return;
+void VolumeTracker::setVolume(uint8_t volume) {
+    if (0 > volume && volume > 100) return;
     this->volume = volume;
 }
 
-void VolumeTracker::mute()
-{
-    muted = true;
-}
+void VolumeTracker::mute() { muted = true; }
 
-void VolumeTracker::unmute()
-{
-    muted = false;
-}
+void VolumeTracker::unmute() { muted = false; }
 
-void VolumeTracker::toggleMute()
-{
-    muted = !muted;
-}
+void VolumeTracker::toggleMute() { muted = !muted; }
--- PIO-Code/src/util/VolumeTracker.h
+++ PIO-Code/src/util/VolumeTracker.h
@@ -3,50 +3,51 @@
 #include <Arduino.h>
 
 class VolumeTracker {
-    private:
-        String name;
-        uint8_t volume = 50;
-        bool muted = false;
-    public:
-        VolumeTracker(String name, uint8_t volume = 50, bool mute = false);
+   private:
+    String name;
+    uint8_t volume = 50;
+    bool muted = false;
 
-        String getName();
+   public:
+    VolumeTracker(String name, uint8_t volume = 50, bool mute = false);
 
-        void setName(String name);
-        /**
-         * @returns current volume
-         */
-        uint8_t getVolume();
+    String getName();
 
-        /**
-         * @returns if volume is muted
-         */
-        bool isMuted();
+    void setName(String name);
+    /**
+     * @returns current volume
+     */
+    uint8_t getVolume();
 
+    /**
+     * @returns if volume is muted
+     */
+    bool isMuted();
 
-        /**
-         * adds the given value to volume with overflow protection
-         */
-        void addVolume(int8_t value);
+    /**
+     * adds the given value to volume with overflow protection
+     */
+    void addVolume(int8_t value);
 
-        /**
-         * set volume to given volume, fails if given volume isnt in range of [0, 100]
-         */
-        void setVolume(uint8_t volume);
+    /**
+     * set volume to given volume, fails if given volume isnt in range of [0,
+     * 100]
+     */
+    void setVolume(uint8_t volume);
 
-        /**
-         * switched mute state;
-         */
-        void toggleMute();
+    /**
+     * switched mute state;
+     */
+    void toggleMute();
 
-        /**
-         * mutes the sound if unmuted before, otherwise does nothing
-         */
-        void mute();
+    /**
+     * mutes the sound if unmuted before, otherwise does nothing
+     */
+    void mute();
 
-        /**
-         * unmutes the sound if muted before, otherwise does nothing
-         */
-        void unmute();
+    /**
+     * unmutes the sound if muted before, otherwise does nothing
+     */
+    void unmute();
 };
 #endif
